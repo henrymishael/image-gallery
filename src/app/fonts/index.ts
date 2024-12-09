546940452fb1cd3427aafb46f7bea31e4d8f5ffb
henrymishael <henrymishael.hm@gmail.com>
2024-12-10 00:53:22 +0100

    first commit~

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,31 @@
+import { Quicksand, Inter } from "next/font/google";
+
+export const quicksand = Quicksand({
+  weight: ["300", "400", "500", "600", "700"],
+  style: "normal",
+  display: "auto",
+  variable: "--font-quicksand",
+  subsets: ["latin", "latin-ext", "vietnamese"],
+  fallback: ["manrope"],
+  preload: true,
+  adjustFontFallback: true,
+});
+
+export const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: "normal",
+  display: "auto",
+  variable: "--font-inter",
+  subsets: [
+    "latin",
+    "latin-ext",
+    "vietnamese",
+    "cyrillic",
+    "cyrillic-ext",
+    "greek-ext",
+    "greek",
+  ],
+  fallback: ["manrope"],
+  preload: true,
+  adjustFontFallback: true,
+});
